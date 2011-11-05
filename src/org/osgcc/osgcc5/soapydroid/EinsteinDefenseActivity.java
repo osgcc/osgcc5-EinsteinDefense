8dfe78f427aaf6c38275929762a2a12d88ad053f
Alexander Conrad <alexander.p.conrad@gmail.com>
2011-11-05 18:38:38 -0400

    initial commit

---
+++ src/org/osgcc/osgcc5/soapydroid/EinsteinDefenseActivity.java
@@ -0,0 +1,13 @@
+package org.osgcc.osgcc5.soapydroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EinsteinDefenseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
