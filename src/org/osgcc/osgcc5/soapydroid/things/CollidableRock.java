030fce85c691deae3e20dc035d31f687f156e62b
unknown <owner@owner-PC.(none)>
2011-11-06 13:02:39 -0500

    Pics, Finished with lvl loader set up, things classes done

--- src/org/osgcc/osgcc5/soapydroid/things/CollidableRock.java
+++ src/org/osgcc/osgcc5/soapydroid/things/CollidableRock.java
@@ -7,17 +7,16 @@ public class CollidableRock extends CollidableThing {
 
 	public CollidableRock(String size) {
 		super();
-<<<<<<< HEAD
+
 		if(size.equals("small"))
 			setBitmap(EinsteinDefenseActivity.getImageCache().get(R.drawable.tree_small));
 		else if(size.equals("medium"))
 			setBitmap(EinsteinDefenseActivity.getImageCache().get(R.drawable.tree));
 		else
 			setBitmap(EinsteinDefenseActivity.getImageCache().get(R.drawable.tree_large));
-=======
+
 		setType("rock");
 		setBitmap(EinsteinDefenseActivity.getImageCache().get(R.drawable.rock));
->>>>>>> 939a6790995d94072e01428073903deec5b7e062
 		isEnemy = false;
 	}
 	
